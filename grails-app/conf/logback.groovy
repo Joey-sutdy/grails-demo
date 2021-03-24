7088f8d96fc702c52deb17ef750d06fe5c500a74
mei <mei.li@convertlab.com>
2021-03-24 17:00:24 +0800

    logback日志配置

--- grails-app/conf/logback.groovy
+++ grails-app/conf/logback.groovy
@@ -1,37 +1,96 @@
+import ch.qos.logback.classic.AsyncAppender
+import ch.qos.logback.classic.encoder.PatternLayoutEncoder
+import ch.qos.logback.core.util.FileSize
 import grails.util.BuildSettings
 import grails.util.Environment
+import groovy.json.JsonBuilder
 import org.springframework.boot.logging.logback.ColorConverter
-import org.springframework.boot.logging.logback.WhitespaceThrowableProxyConverter
 
-import java.nio.charset.StandardCharsets
+conversionRule("clr", ColorConverter)
 
-conversionRule 'clr', ColorConverter
-conversionRule 'wex', WhitespaceThrowableProxyConverter
+def LOG_LEVEL_PATTERN = System.getProperty("LOG_LEVEL_PATTERN")?:'%5p'
+def PID = System.getProperty("PID")?:"- "
+def LOG_EXCEPTION_CONVERSION_WORD = System.getProperty("LOG_EXCEPTION_CONVERSION_WORD")?:'%xEx'
 
-// See http://logback.qos.ch/manual/groovy.html for details on configuration
-appender('STDOUT', ConsoleAppender) {
+def CONSOLE_LOG_PATTERN = "%clr(%d{yyyy-MM-dd HH:mm:ss.SSS}){faint} " +
+        "%clr(${LOG_LEVEL_PATTERN}) %clr(${PID}){magenta} " +
+        "%clr(---){faint} %clr([%15.15t]){faint}" +
+        "%clr(%-40.40logger{39}){cyan} " +
+        "%clr(:){faint} %m%n${LOG_EXCEPTION_CONVERSION_WORD}"
+
+appender('CONSOLE', ConsoleAppender) {
     encoder(PatternLayoutEncoder) {
-        charset = StandardCharsets.UTF_8
-
-        pattern =
-                '%clr(%d{yyyy-MM-dd HH:mm:ss.SSS}){faint} ' + // Date
-                        '%clr(%5p) ' + // Log level
-                        '%clr(---){faint} %clr([%15.15t]){faint} ' + // Thread
-                        '%clr(%-40.40logger{39}){cyan} %clr(:){faint} ' + // Logger
-                        '%m%n%wex' // Message
+        pattern = CONSOLE_LOG_PATTERN
     }
 }
 
-def targetDir = BuildSettings.TARGET_DIR
-if (Environment.isDevelopmentMode() && targetDir != null) {
-    appender("FULL_STACKTRACE", FileAppender) {
-        file = "${targetDir}/stacktrace.log"
-        append = true
-        encoder(PatternLayoutEncoder) {
-            charset = StandardCharsets.UTF_8
-            pattern = "%level %logger - %msg%n"
+def env = System.getenv()
+def service = env['SERVICE_NAME'] ?: 'demo-limei'
+def logdir = (env.LOG_DIR ?: "/opt/log/stash") + "/${service}"
+def logFile = "${logdir}/${service}.log"
+
+def additionalFields = new JsonBuilder([
+        "env": System.getProperty("grails.env"),
+        "service": service,
+        "su": env["service_unit_key"]?:'',
+]).toPrettyString()
+
+
+def var =appender("FILE", RollingFileAppender) {
+    file = logFile
+    encoder(net.logstash.logback.encoder.LogstashEncoder) {
+        customFields = additionalFields
+        includeMdc = true
+    }
+    rollingPolicy(TimeBasedRollingPolicy) {
+        fileNamePattern = "${logdir}/${service}-%d{yyyy-MM-dd-HH}.gz"
+        maxHistory = 48
+        totalSizeCap = FileSize.valueOf("64GB")
+    }
+}
+
+
+
+appender("ASYNC", AsyncAppender) {
+    appenderRef("FILE")
+    queueSize = 524288
+    discardingThreshold = 32768
+}
+
+appender('CONSOLE_JSON', ConsoleAppender) {
+    encoder(net.logstash.logback.encoder.LogstashEncoder) {
+        customFields = additionalFields
+        includeMdc = true
+    }
+}
+
+if(Environment.current == Environment.DEVELOPMENT) {
+    def targetDir = BuildSettings.TARGET_DIR
+    if(targetDir) {
+
+        appender("FULL_STACKTRACE", FileAppender) {
+
+            file = "${targetDir}/stacktrace.log"
+            append = true
+            encoder(PatternLayoutEncoder) {
+                pattern = "%level %logger - %msg%n"
+            }
         }
+        logger("StackTrace", ERROR, ['FULL_STACKTRACE'], false )
     }
-    logger("StackTrace", ERROR, ['FULL_STACKTRACE'], false)
+    root(INFO, ["CONSOLE", "ASYNC"])
+
+} else if (env.LOG_TO_CONSOLE) {
+    if (env.LOG_USE_JSON) {
+        root(INFO, ["CONSOLE_JSON"])
+    } else {
+        root(INFO, ["CONSOLE"])
+    }
+} else {
+    root(INFO, ["ASYNC"])
 }
-root(ERROR, ['STDOUT'])
+
+logger("org.springframework.boot.autoconfigure.security", INFO)
+logger("org.springframework.security.web.authentication", OFF)
+
+logger('org.apache.kafka.clients.FetchSessionHandler', WARN)
--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,3 +1,10 @@
+import com.convertlab.multitenancy.CurrentTenantThreadLocal
+import com.convertlab.multitenancy.resolver.ThreadLocalTenantResolver
+
 // Place your Spring DSL code here
+
+
 beans = {
+    currentTenant(CurrentTenantThreadLocal)
+    tenantResolver(ThreadLocalTenantResolver)
 }
